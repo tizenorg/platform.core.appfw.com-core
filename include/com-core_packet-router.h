c79fbde41343e9fe4edf5bb1bdeed0bfe2972e00
Sung-jae Park <nicesj.park@samsung.com>
2012-12-19 17:16:09 +0900

    Update license for uploading to RSA
    Change-Id: Iaf3dc7c72b18bfeffca888966515b2e2a6a5a9cd

--- include/com-core_packet-router.h
+++ include/com-core_packet-router.h
@@ -1,8 +1,17 @@
-/*!
- * \NOTE
- * This component uses THREAD.
- * If you want to keep your application running without multiple threads,
- * DO NOT USE THIS.
+/*
+ * Copyright 2012  Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Flora License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.tizenopensource.org/license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 enum com_core_route_event_type {
--- packaging/libcom-core.spec
+++ packaging/libcom-core.spec
@@ -1,6 +1,6 @@
 Name: libcom-core
 Summary: Library for the light-weight IPC 
-Version: 0.3.4
+Version: 0.3.5
 Release: 1
 Group: main/util
 License: Flora License
