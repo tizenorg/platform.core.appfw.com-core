5aa6fdab8e10588513bbc96b3c432227f0ef1309
Sung-jae Park <nicesj.park@samsung.com>
2013-09-26 10:15:01 +0900

    Replace gettimeofday with clock_gettime
    Change-Id: Ice0e00e2ffbba6f5a31012b56cf6df0f4884db95

--- src/util.c
+++ src/util.c
@@ -17,12 +17,23 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <errno.h>
+#include <time.h>
+#include <dlog.h>
 
+#include "debug.h"
 #include "util.h"
 
 int errno;
+#if defined(_USE_ECORE_TIME_GET)
+static struct {
+	clockid_t type;
+} s_info = {
+	.type = CLOCK_MONOTONIC,
+};
+#endif
 
 const char *util_basename(const char *name)
 {
@@ -34,6 +45,38 @@ const char *util_basename(const char *name)
 	while (--length > 0 && name[length] != '/');
 
 	return length <= 0 ? name : name + length + (name[length] == '/');
+}
+
+double util_timestamp(void)
+{
+#if defined(_USE_ECORE_TIME_GET)
+	struct timespec ts;
+
+	do {
+		if (clock_gettime(s_info.type, &ts) == 0) {
+			return ts.tv_sec + ts.tv_nsec / 1000000000.0f;
+		}
+
+		ErrPrint("%d: %s\n", s_info.type, strerror(errno));
+		if (s_info.type == CLOCK_MONOTONIC) {
+			s_info.type = CLOCK_REALTIME;
+		} else if (s_info.type == CLOCK_REALTIME) {
+			break;
+		}
+	} while (1);
+
+	return 0.0f;
+#else
+	struct timeval tv;
+
+	if (gettimeofday(&tv, NULL) < 0) {
+		ErrPrint("gettimeofday: %s\n", strerror(errno));
+		tv.tv_sec = rand();
+		tv.tv_usec = rand();
+	}
+
+	return (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0f;
+#endif
 }
 
 /* End of a file */
